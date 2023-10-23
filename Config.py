e18868376586a57140b335c3864417d3e96cd325
Nbt guy <93465296+Nbtguyoriginal@users.noreply.github.com>
2023-10-22 22:53:48 -0400

    Create Config.py
    Basic configuration and json paths

---
+++ Config.py
@@ -0,0 +1,13 @@
+# Configuration for the game
+
+# OpenAI API Key
+OPENAI_API_KEY = "YOUR_OPENAI_API_KEY"
+
+# Directories for JSON data
+PLAYER_DIR = "data/players/"
+ENEMY_DIR = "data/enemies/"
+NPC_DIR = "data/npcs/"
+LOCATION_DIR = "data/locations/"
+QUEST_DIR = "data/quests/"
+SPELL_DIR = "data/spells/"
+GAME_STATE_DIR = "data/game_states/"
