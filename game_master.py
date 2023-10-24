629233e1a8068c4e5619f1c16b6e84214445be81
Nbt guy <93465296+Nbtguyoriginal@users.noreply.github.com>
2023-10-24 19:05:58 -0400

    Update game_master.py
    yet another rewrite to add the logic or these 
    
    """
    -Attributes for the game state.
    -The missing_info_queue attribute to keep track of what 
     information needs to be requested.
    -The check_and_add_to_queue() function which adds missing 
     information to the queue.
    -The group_missing_info() function that tries to group related 
     missing information to reduce the number of requests.
    -The construct_request_for_missing_info() function was adapted to utilize the queue, perform grouping, and then construct a request using example formats.
    """

--- game_master.py
+++ game_master.py
@@ -1,4 +1,6 @@
 import json
+from collections import deque, OrderedDict
+from patterns import example_formats  # Importing the formats
 
 class GameMaster:
     def __init__(self):
@@ -23,6 +25,7 @@ class GameMaster:
         self.quest = None
         self.commands = None
         self.action_summary = []
+        self.missing_info_queue = deque()
 
     def update_game_state(self, game_state):
         for key, value in game_state.items():
@@ -46,15 +49,37 @@ class GameMaster:
         missing_info = [info for info in essential_info if getattr(self, info, None) is None]
         return missing_info
 
+    def check_and_add_to_queue(self, missing_info):
+        for info in missing_info:
+            if info not in self.missing_info_queue:
+                self.missing_info_queue.append(info)
+
+    def group_missing_info(self):
+        grouping = [
+            ['turn', 'time', 'day', 'weather'],
+            ['health', 'xp', 'ac', 'level'],
+            ['location', 'gold', 'silver', 'bronze'],
+            ['wearing', 'wielding', 'status_effects'],
+            ['description', 'inventory', 'abilities', 'quest', 'commands']
+        ]
+        for group in grouping:
+            if all(item in self.missing_info_queue for item in group):
+                for item in group:
+                    self.missing_info_queue.remove(item)
+                return group
+        return [self.missing_info_queue.popleft()]
+
     def construct_request_for_missing_info(self):
         missing_info = self.is_info_missing()
-        if missing_info:
-            request = f"GameMaster, we are missing information on the following: {', '.join(missing_info)}. " \
-                      f"Can you provide the details?"
-            return request
-        return "All essential information is available."
+        self.check_and_add_to_queue(missing_info)
+        if not self.missing_info_queue:
+            return "All essential information is available."
 
-# Example usage
+        current_group = self.group_missing_info()
+        examples = [example_formats[item] for item in current_group]
+        request = f"GameMaster, we need details in the format of: {', '.join(examples)}."
+        return request
+
 if __name__ == "__main__":
     game_master = GameMaster()
     game_master.weather = None  # Simulating missing information
